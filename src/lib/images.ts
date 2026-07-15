d6830c31c509994c22456a4863e5cdcc40aa5b18
elio-rivas <eliocarmen@hotmail.com>
2026-07-14 18:17:47 -0600

    hidding summer camp and replacing pic from home about something isn't right

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -5,7 +5,7 @@
 export const siteImages = {
   homeHero:        'https://images.pexels.com/photos/6654125/pexels-photo-6654125.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&fit=crop',
   summerSection:   'https://images.pexels.com/photos/9127717/pexels-photo-9127717.jpeg?auto=compress&cs=tinysrgb&w=800&h=600&fit=crop',
-  parentConcern:   'https://images.pexels.com/photos/6186124/pexels-photo-6186124.jpeg?auto=compress&cs=tinysrgb&w=700&h=500&fit=crop',
+  parentConcern:   '/images/services/Screenshot_2026-07-14_at_6.08.36_PM.png',
   summerPageHero:  'https://images.pexels.com/photos/8205393/pexels-photo-8205393.jpeg?auto=compress&cs=tinysrgb&w=1920&h=800&fit=crop',
   aboutFeature:    'https://images.pexels.com/photos/18830100/pexels-photo-18830100.jpeg?auto=compress&cs=tinysrgb&w=700&h=500&fit=crop',
   servicesBanner:  'https://images.pexels.com/photos/8363091/pexels-photo-8363091.jpeg?auto=compress&cs=tinysrgb&w=900&h=400&fit=crop',
